2e88ed951287caf0a89b6b45c1e5988600e74164
Gr8V <vanshdangi06@gmail.com>
2026-01-14 19:44:36 +0530

    added comments to daily data model

--- fuel_iq/lib/models/daily_data_model.dart
+++ fuel_iq/lib/models/daily_data_model.dart
@@ -6,19 +6,19 @@ class DailyDataModel {
   WorkoutData workout;
   CardioData cardio;
 
-  List<String> supplements;
+  Map<String, bool> supplements;
   double weight;
 
   DailyDataModel({
     NutritionData? nutrition,
     WorkoutData? workout,
     CardioData? cardio,
-    List<String>? supplements,
+    Map<String, bool>? supplements,
     this.weight = 0.0,
   })  : nutrition = nutrition ?? NutritionData(),
         workout = workout ?? WorkoutData(),
         cardio = cardio ?? CardioData(),
-        supplements = supplements ?? [];
+        supplements = supplements ?? {};
 
   Map<String, dynamic> toJson() => {
         'nutrition': nutrition.toJson(),
@@ -39,8 +39,51 @@ class DailyDataModel {
       cardio: CardioData.fromJson(
         Map<String, dynamic>.from(json['cardio'] ?? {}),
       ),
-      supplements: List<String>.from(json['supplements'] ?? []),
+      supplements: Map<String, bool>.from(
+        json['supplements'] ?? {},
+      ),
       weight: (json['weight'] ?? 0).toDouble(),
     );
   }
 }
+
+
+/*
+DAILY DATA MODAL JSON VISUALIZATION
+{
+  "nutrition": {
+    "calories": 0.0,
+    "protein": 0.0,
+    "carbs": 0.0,
+    "fats": 0.0,
+    "water": 3.0,
+    "targets": {
+      "calories": 2300.0,
+      "protein": 150.0,
+      "carbs": 250.0,
+      "fats": 70.0,
+      "water": 3.0
+    },
+    "foods": []
+  },
+
+  "workout": {
+    "name": "Push Day",
+    "exercises": [],
+    "duration": 0
+  },
+
+  "cardio": {
+    "type": "Walking",
+    "steps": 0,
+    "calories": 0
+  },
+
+  "supplements": {
+    "Creatine": true,
+    "Whey": false,
+    "Multivitamin": true
+  }
+
+  "weight": 0.0
+}*/
